67cb8035c9b5e393526f53a86546fe4e2e2425b6
Ahmebis <ahmedemada83@gmail.com>
2025-09-24 09:49:21 +0300

    feat: add pagination feature to product listing

--- src/app/(pages)/[ProductId]/page.tsx
+++ src/app/(pages)/[ProductId]/page.tsx
@@ -45,56 +45,88 @@ export default async function ProductDetails({ params }: { params: Params }) {
     );
   }
 
-  return (
-    <Card className="grid md:grid-cols-3 items-center">
-      <div className="col-span-1">
-        <Carousel className="w-full max-w-md">
-          <CarouselContent>
-            {product.images?.map((img, index) => (
-              <CarouselItem key={index}>
-                <Image
-                  src={img}
-                  alt={`Product image ${index + 1}`}
-                  width={400}
-                  height={400}
-                  className="w-full h-auto object-cover rounded-lg"
-                />
-              </CarouselItem>
-            ))}
-          </CarouselContent>
-          <CarouselPrevious />
-          <CarouselNext />
-        </Carousel>
-      </div>
+  const allProductsRes = await fetch(
+    "https://ecommerce.routemisr.com/api/v1/products?fields=id,title"
+  );
+  const allProductsData = await allProductsRes.json();
+  const allProducts: ProductI[] = allProductsData.data;
 
-      <div className="md:col-span-2 space-y-4">
-        <CardHeader>
-          <CardDescription>{product.brand.name}</CardDescription>
-          <CardTitle className="text-2xl">{product.title}</CardTitle>
-          <CardDescription>{product.description}</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <CardDescription>{product.category.name}</CardDescription>
-          <div className="flex justify-between items-center mt-3 ">
-            <p className="flex gap-1 ">
-              <StarIcons />
-              Rating : <span>{product.ratingsAverage}</span>
-            </p>
-            <p>
-              Remaining :<span>{product.ratingsQuantity}</span>
-            </p>
-          </div>
-          <div className="flex justify-between items-center mt-3">
-            <p className="flex gap-1">
-              Price :<span className="font-bold">{product.price} EGP</span>
-            </p>
-            <p className="flex gap-1">
-              Quantity :<span className="font-bold">{product.quantity}</span>
-            </p>
-          </div>
-        </CardContent>
-        <AddToCart productId={product.id} />
+  const currentProductIndex = allProducts.findIndex((n) => n.id === ProductId);
+
+  const nextProduct = allProducts[currentProductIndex + 1];
+  const prevProduct = allProducts[currentProductIndex - 1];
+
+  return (
+    <>
+      <Card className="grid md:grid-cols-3 items-center">
+        <div className="col-span-1">
+          <Carousel className="w-full max-w-md">
+            <CarouselContent>
+              {product.images?.map((img, index) => (
+                <CarouselItem key={index}>
+                  <Image
+                    src={img}
+                    alt={`Product image ${index + 1}`}
+                    width={400}
+                    height={400}
+                    className="w-full h-auto object-cover rounded-lg"
+                  />
+                </CarouselItem>
+              ))}
+            </CarouselContent>
+            <CarouselPrevious />
+            <CarouselNext />
+          </Carousel>
+        </div>
+
+        <div className="md:col-span-2 space-y-4">
+          <CardHeader>
+            <CardDescription>{product.brand.name}</CardDescription>
+            <CardTitle className="text-2xl">{product.title}</CardTitle>
+            <CardDescription>{product.description}</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <CardDescription>{product.category.name}</CardDescription>
+            <div className="flex justify-between items-center mt-3 ">
+              <p className="flex gap-1 ">
+                <StarIcons />
+                Rating : <span>{product.ratingsAverage}</span>
+              </p>
+              <p>
+                Remaining :<span>{product.ratingsQuantity}</span>
+              </p>
+            </div>
+            <div className="flex justify-between items-center mt-3">
+              <p className="flex gap-1">
+                Price :<span className="font-bold">{product.price} EGP</span>
+              </p>
+              <p className="flex gap-1">
+                Quantity :<span className="font-bold">{product.quantity}</span>
+              </p>
+            </div>
+          </CardContent>
+          <AddToCart productId={product.id} />
+        </div>
+        <div></div>
+      </Card>
+
+      <div className="flex justify-between mt-8 px-4 max-w-4xl mx-auto">
+        {prevProduct ? (
+          <Button asChild variant="outline">
+            <a href={`/${prevProduct.id}`}> Previous</a>
+          </Button>
+        ) : (
+          <div />
+        )}
+
+        {nextProduct ? (
+          <Button asChild variant="outline">
+            <a href={`/${nextProduct.id}`}>Next </a>
+          </Button>
+        ) : (
+          <div />
+        )}
       </div>
-    </Card>
+    </>
   );
 }
